64a8388afafe7b66264208474804491d554cb602
GaelDuclocherRougelot <gaelduclocher.rougelot@gmail.com>
2023-06-05 12:20:48 +0200

    alphabet ok

---
+++ LEVEL1/alphabet.js
@@ -0,0 +1,10 @@
+// ce code doit retrourner l'alphabet sur une meme ligne, suivi d'un retour à la ligne
+
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q', 'r', 's', 't', 'u','v', 'w', 'x', 'y', 'z'];
+
+for (let i = 0; i < alphabet.length; i++) {
+  process.stdout.write(`${alphabet[i]}`);
+  if (alphabet[i] === 'z') {
+    process.stdout.write(`\n`);
+  }
+};
